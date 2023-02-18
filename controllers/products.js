b9ebfd7dfbeaa7c030cf2522d65d4477d6213ffc
surabhi-99 <surabhithakur.33@gmail.com>
2023-02-18 14:33:04 +0530

    first commit

---
+++ controllers/products.js
@@ -0,0 +1,8 @@
+const getAllProducts = async(req, res) =>{
+    res.status(200).json({msg: "I am getAllProducts"});
+};
+const getAllProductsTesting = async(req, res) =>{
+    res.status(200).json({msg: "I am getAllProductsTesting"});
+};
+
+module.exports = { getAllProducts, getAllProductsTesting };
